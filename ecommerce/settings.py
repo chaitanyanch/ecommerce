1a8208f959b3cd9d2c75286f6ceb0c3f0b12bf8e
chaitanyanch <chaitanya4656@gmail.com>
2023-11-18 11:23:56 +0530

    changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     "/var/www/static/",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/app/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
